2f4b08ea50f04878bb0aecc852b6413f8209dcee
Kishor <kishor1312@gmail.com>
2026-02-11 14:34:07 +0530

    [-] snippet dryruns

---
+++ com/TestPalindrome.java
@@ -0,0 +1,43 @@
+package com;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestPalindrome {
+    public static void main(String[] args) {
+        String inputStr = "bananas";
+        List<String> palindromes = new ArrayList<>();
+
+        int i = 0;
+        int j = i + 1;
+        while (i < j && j <= inputStr.length()) {
+            String subStr = inputStr.substring(i, j);
+            System.out.println(subStr+"_"+i+""+j);
+
+            if (j == inputStr.length()) {
+                i++;
+                j = i + 1;
+            }
+            if (isThisPalindrome(subStr)) {
+                palindromes.add(subStr);
+            }
+            j++;
+        }
+        System.out.println("checking list: "+palindromes.size());
+        palindromes.forEach(s -> System.out.println(s));
+    }
+
+    private static boolean isThisPalindrome(String substring) {
+        // return substring.equals((new StringBuffer(substring)).reverse().toString());
+        int left = 0;
+        int right = substring.length() - 1;
+        while (left < right && substring.charAt(left) == substring.charAt(right)) {
+            left++;
+            right--;
+            if(right <= left) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ com/TestReverseInteger.java
@@ -0,0 +1,40 @@
+package com;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+public class TestReverseInteger {
+
+    // basic with mutating original value
+    /*
+    public static void main(String[] args) {
+        int inputInt = 323021;
+        int reducedInt = 0;
+        int reversedInt = 0;
+        do {
+            int modVal;
+            if (reducedInt == 0) {
+                modVal = inputInt % 10;
+                reducedInt = inputInt / 10;
+            } else {
+                modVal = reducedInt % 10;
+                reducedInt = reducedInt / 10;
+            }
+            reversedInt = reversedInt * 10 + modVal;
+        } while (reducedInt > 10);
+        System.out.println(reversedInt * 10 + reducedInt);
+    } */
+
+    //with mutation of original value
+    public static void main(String[] args) {
+        int inputInt = 12340;
+        int temp = inputInt;
+        int reversedInt = 0;
+        while(temp != 0) {
+            int modVal = temp % 10;
+            reversedInt = reversedInt * 10 + modVal;
+            temp = temp / 10;
+        }
+        System.out.println(reversedInt);
+    }
+}
