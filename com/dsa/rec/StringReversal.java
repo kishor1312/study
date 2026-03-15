910c6de8d9d8f6e57a6848da6bed0d6107b64d94
Kishor <kishor1312@gmail.com>
2026-03-15 13:18:09 +0530

    [-] print string reversal using recursion

---
+++ com/dsa/rec/StringReversal.java
@@ -0,0 +1,34 @@
+package com.dsa.rec;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class StringReversal {
+    static String inputStr;
+    public static void main(String[] args) {
+        inputStr = new Scanner(System.in).nextLine();
+
+        //copy string in charArray
+        char[] arr = inputStr.toCharArray();
+
+        //pass initial arguments
+        reverseArr(arr, 0, arr.length -1);
+
+        //print final result
+        System.out.println("reverse of given string is : "+String.copyValueOf(arr));
+    }
+
+    private static void reverseArr(char[] arr, int left, int right) {
+        //exit condition
+        if(left >=right)
+            return;
+
+        // logic
+        char temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+
+        //recursion
+        reverseArr(arr, ++left, --right);
+    }
+}
